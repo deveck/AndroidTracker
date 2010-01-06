4f2bd275058a0aa1ba041fab7bb58ebb90a561dc
unknown <Administrator@.(none)>
2010-01-06 21:01:47 +0430

    icon, text

--- AndroidTracker/gen/org/cw/R.java
+++ AndroidTracker/gen/org/cw/R.java
@@ -11,7 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int application_logo=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int buttonOk=0x7f050004;
